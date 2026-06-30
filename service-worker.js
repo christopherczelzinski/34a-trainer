52a8f925bdba685fd634d074cda4fb95aac606ec
christopherczelzinski <christopher.czelzinski@gmail.com>
2026-06-30 12:12:02 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+
+  /* 34a-trainer-v2 oder erhöhen */ 
+const CACHE_NAME = "34a-trainer-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.json",
+                "./icons/icon-192.png",
+                "./icons/icon-512.png"
+            ]);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
