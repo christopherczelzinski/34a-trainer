a8271cd7653e2531c200655448f3e69ce2a18b24
christopherczelzinski <christopher.czelzinski@gmail.com>
2026-07-01 10:04:55 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
   /* 34a-trainer-v2 oder erhöhen */ 
-const CACHE_NAME = "34a-trainer-v1";
+const CACHE_NAME = "34a-trainer-v2";
 
 self.addEventListener("install", event => {
     event.waitUntil(
